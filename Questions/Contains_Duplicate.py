b64e175ab8e3d925bb74acab351124e21f93d98f
Anthony Lam <99anthonylam@gmail.com>
2019-08-14 20:03:03 -0700

    Add Contains Duplicate

---
+++ Questions/Contains_Duplicate.py
@@ -0,0 +1,8 @@
+# Runtime: 132 ms, faster than 97.60% of Python3 online submissions for Contains Duplicate.
+# Memory Usage: 19.2 MB, less than 16.98% of Python3 online submissions for Contains Duplicate.
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 0:
+            return False
+        return len(nums) != len(set(nums))
