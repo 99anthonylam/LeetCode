61002bb3134282cacae56d8c7a0421bb29a74307
Anthony Lam <99anthonylam@gmail.com>
2019-08-14 19:54:10 -0700

    Format files

--- Questions/Best_Time_To_Buy_And_Sell_Stock.py
+++ Questions/Best_Time_To_Buy_And_Sell_Stock.py
@@ -1,5 +1,6 @@
 # Runtime: 72 ms, faster than 78.00% of Python3 online submissions for Best Time to Buy and Sell Stock.
 # Memory Usage: 14.9 MB, less than 5.75% of Python3 online submissions for Best Time to Buy and Sell Stock.
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if (len(prices) == 0):
--- Questions/Two_Sum.py
+++ Questions/Two_Sum.py
@@ -1,5 +1,6 @@
 # Runtime: 52 ms, faster than 96.32% of Python3 online submissions for Two Sum.
 # Memory Usage: 15 MB, less than 9.06% of Python3 online submissions for Two Sum.
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = {}
