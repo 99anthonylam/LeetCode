695181bbb614fe840c4b8d83c1c115b72dfcd714
Cindy Zhang <cindyz8735@gmail.com>
2019-08-16 09:12:41 -0700

    update readme

--- Questions/Product_Of_Array_Except_Self.py
+++ Questions/Product_Of_Array_Except_Self.py
@@ -7,9 +7,9 @@ class Solution:
         left_prod = right_prod = 1
         
         # multiply from left side
-        for val in nums:
+        for i in range(len(nums)):  # faster than for each loop
             output.append(left_prod)
-            left_prod *= val
+            left_prod *= nums[i]
         
         # multiply from right side
         for i in range(len(nums)-1, -1, -1):
