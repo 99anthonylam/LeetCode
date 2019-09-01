5e815766a4217bc6c0c411457bc8b5f7569a7142
anthony <99anthonylam@gmail.com>
2019-09-01 00:40:47 -0400

    Fix contains dupe

--- Questions/Array/Contains_Duplicate.py
+++ Questions/Array/Contains_Duplicate.py
@@ -1,3 +1,6 @@
+# Link: https://leetcode.com/problems/contains-duplicate/
+# Difficulty: Easy
+
 # Runtime: 132 ms, faster than 97.60% of Python3 online submissions for Contains Duplicate.
 # Memory Usage: 19.2 MB, less than 16.98% of Python3 online submissions for Contains Duplicate.
 
